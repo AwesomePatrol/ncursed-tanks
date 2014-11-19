c2e286cc9c628d3aca4b7ca2798de236f8ba4706
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2014-11-19 18:24:55 +0100

    Fix indentation in client.c

--- client.c
+++ client.c
@@ -2,26 +2,26 @@
 
 int main(int argc, char *argv[])
 {
-   char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-   int len, mysocket;
-   struct sockaddr_in dest; 
+    char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
+    int len, mysocket;
+    struct sockaddr_in dest; 
  
-   mysocket = socket(AF_INET, SOCK_STREAM, 0);
+    mysocket = socket(AF_INET, SOCK_STREAM, 0);
  
-   memset(&dest, 0, sizeof(dest));                /* zero the struct */
-   dest.sin_family = AF_INET;
-   dest.sin_addr.s_addr = htonl((int) argv[1]); /* set destination IP number */ 
-   dest.sin_port = htons(PORTNUM);                /* set destination port number */
+    memset(&dest, 0, sizeof(dest));              /* zero the struct */
+    dest.sin_family = AF_INET;
+    dest.sin_addr.s_addr = htonl((int) argv[1]); /* set destination IP number */
+    dest.sin_port = htons(PORTNUM);              /* set destination port number */
  
-   connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
+    connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr));
  
-   len = recv(mysocket, buffer, MAXRCVLEN, 0);
+    len = recv(mysocket, buffer, MAXRCVLEN, 0);
  
-   /* We have to null terminate the received data ourselves */
-   buffer[len] = '\0';
+    /* We have to null terminate the received data ourselves */
+    buffer[len] = '\0';
  
-   if ( DEBUG <= 3 ) printf("Received %s (%d bytes).\n", buffer, len);
+    if ( DEBUG <= 3 ) printf("Received %s (%d bytes).\n", buffer, len);
  
-   close(mysocket);
-   return EXIT_SUCCESS;
+    close(mysocket);
+    return EXIT_SUCCESS;
 }
