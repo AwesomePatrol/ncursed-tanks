054151e9c7e5933e2858fc3e5f9938f0d5265eb9
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2014-11-21 20:11:48 +0100

    server: Add SIGINT handling, separate cleanup (restoring SIGINT handler after receiving the signal doesn't work yet)

--- server.c
+++ server.c
@@ -1,42 +1,22 @@
 #include "server.h"
 
 int mysocket;            /* socket used to listen for incoming connections */
+int consocket;
+
+struct sigaction sigaction_prev;
 
 int map_seed;
 map_t map = NULL;
 int map_length = 80, map_height = 24;
 
-void process_request(char *request)
-{
-    if (strlen(request) < 1)
-    {
-        if (DEBUG <= 5) puts("Received data length is <1 !");
-        return;
-    }
-
-    Command cmd = request[0];
-    char answer[MAXRCVLEN + 1];
-
-    switch(cmd)
-    {
-    case GET_MAP:
-        /* TODO send */
-        map_seed = rand();
-        if (DEBUG == 0) printf("Map seed is %d\n", map_seed);
-
-        snprintf(answer, MAXRCVLEN + 1, "%d", map_seed);
-        send(mysocket, answer, strlen(answer), 0);
-
-        map = generate_map(map_seed, map_length, map_height);
-
-        break;
-    default:
-        if (DEBUG <= 5) printf("Unrecognized command from client: '%c' !", cmd);
-    }
-}
+void exit_cleanup(void);
+void sigint_handler(int signum);
+void process_request(char *request);
 
 int main(int argc, char *argv[])
 {
+    struct sigaction sigaction_new = {0};
+
     unsigned int random_seed;
 
     char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
@@ -45,6 +25,17 @@ int main(int argc, char *argv[])
     struct sockaddr_in dest; /* socket info about the machine connecting to us */
     struct sockaddr_in serv; /* socket info about our server */
     socklen_t socksize = sizeof(struct sockaddr_in);
+
+    /* Cleanup on normal exit */
+    if (atexit(exit_cleanup) != 0)
+        if (DEBUG <= 5) puts(
+"Warning! Couldn't register exit handler. Won't be able to clean up on exit!");
+    /* Handle SIGINT (Control-c) */
+    sigaction_new.sa_handler = sigint_handler;
+    if (sigaction(SIGINT, &sigaction_new, &sigaction_prev) != 0)
+        if (DEBUG <= 5) puts(
+"Warning! Couldn't assign handler to SIGINT. If the server is terminated,\n"
+"won't be able to clean up!");
 
     random_seed = time(NULL);
     if (DEBUG == 0) printf("Initial random seed is %u\n", random_seed);
@@ -66,7 +57,7 @@ int main(int argc, char *argv[])
  
     /* start listening, allowing a queue of up to 16 pending connection */
     listen(mysocket, 16);
-    int consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
+    consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
  
     if (consocket)
     {
@@ -83,7 +74,53 @@ int main(int argc, char *argv[])
         if (DEBUG <= 5) printf("Couldn't accept connection!\n");
     }
 
+    return EXIT_SUCCESS;
+}
+
+
+void exit_cleanup(void)
+{
+    if (map) free(map);
     close(consocket);
     close(mysocket);
-    return EXIT_SUCCESS;
+}
+
+void sigint_handler(int signum)
+{
+    if (DEBUG <= 3) puts(
+"Received SIGINT, cleaning up...\n"
+"Another SIGINT will terminate the server immediately");
+
+    sigaction(SIGINT, &sigaction_prev, NULL);
+
+    exit(EXIT_SUCCESS);
+}
+
+void process_request(char *request)
+{
+    if (strlen(request) < 1)
+    {
+        if (DEBUG <= 5) puts("Received data length is <1 !");
+        return;
+    }
+
+    Command cmd = request[0];
+    char answer[MAXRCVLEN + 1];
+
+    switch (cmd)
+    {
+    case GET_MAP:
+        /* TODO send */
+        map_seed = rand();
+        if (DEBUG == 0) printf("Map seed is %d\n", map_seed);
+
+        snprintf(answer, MAXRCVLEN + 1, "%d", map_seed);
+        send(mysocket, answer, strlen(answer), 0);
+
+        map = generate_map(map_seed, map_length, map_height);
+
+        break;
+    default:
+        if (DEBUG <= 5) printf("Unrecognized command from client: '%c' !", cmd);
+    }
 }
--- server.h
+++ server.h
@@ -1,10 +1,13 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#define _POSIX_C_SOURCE 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <netinet/in.h>
