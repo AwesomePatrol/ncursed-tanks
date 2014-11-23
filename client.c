8094fe124524b64d89634bb6aa3332218cf272b8
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2014-11-23 21:05:58 +0100

    client: Add more debug printing, fix error-checking

--- client.c
+++ client.c
@@ -24,14 +24,18 @@ int main(int argc, char *argv[])
 {
     /* Be sure we have proper arguments */
     if ( argc <= 2 || strlen(argv[1]) < 7
-            || strlen(argv[2]) < 3) return EXIT_FAILURE;
+            || strlen(argv[2]) < 3)
+    {
+        if (DEBUG <= 5) puts("Wrong command-line arguments!");
+        return EXIT_FAILURE;
+    }
 
     /* Init ncurses */
     init_curses();
     
     /* Get connection to server */
     char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-    int len, cl_sock;
+    int len, sent, cl_sock;
     struct sockaddr_in dest; 
  
     cl_sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -44,14 +48,25 @@ int main(int argc, char *argv[])
     
     /* set destination IP number */ 
     dest.sin_addr.s_addr = inet_addr(argv[1]);
-   
+
     /* set destination port number */
     dest.sin_port = htons(PORTNUM);
- 
+
     /* in case socket or connection is broken we should fail here */
     if ( cl_sock == -1 ||
-        connect(cl_sock, (struct sockaddr *)&dest, sizeof(struct sockaddr)) )
-            return EXIT_FAILURE;
+         connect(cl_sock, (struct sockaddr *)&dest, sizeof(struct sockaddr))
+         == -1 )
+    {
+        if (DEBUG <= 5) puts("Socket or connection is broken!");
+        return EXIT_FAILURE;
+    }
+
+    sent = send(cl_sock, "M", 1, 0);
+    if (DEBUG <= 3)
+    {
+        printw("Sent %d bytes.", sent);
+        getch();
+    }
 
     /* Close connection */
     close(cl_sock);
