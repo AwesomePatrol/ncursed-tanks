15f1b3ff6d849064807c01157f3ecf0cd260774b
Aleksander Mistewicz <a.mistewicz@gmail.com>
2014-11-24 09:34:02 +0100

    Fix typo

--- cl_proto.c
+++ cl_proto.c
@@ -4,10 +4,10 @@ void fetch_map(int sock)
 {
     debug_d( 3, "bytes sent",
             send( sock, "M", 1, 0));
-    debug_d( 3, "bytes recived",
+    debug_d( 3, "bytes received",
             recv( sock, &map_seed, sizeof(int), 0));
-    debug_d( 3, "bytes recived",
+    debug_d( 3, "bytes received",
             recv( sock, &map_length, sizeof(int), 0));
-    debug_d( 3, "bytes recived",
+    debug_d( 3, "bytes received",
             recv( sock, &map_height, sizeof(int), 0));
 }
