3a9d3db58d2f2f66411505eab35c8d4355c7cf86
Aleksander Mistewicz <a.mistewicz@gmail.com>
2014-11-25 00:37:40 +0100

    Receive map_info with fields in network byte order.

--- cl_proto.c
+++ cl_proto.c
@@ -1,8 +1,16 @@
 #include "client.h"
 
+struct map_info net_to_map_info(struct map_info *i)
+{
+    return
+        (struct map_info) {ntohl(i->seed), ntohs(i->length), ntohs(i->height)};
+}
+
 void fetch_map(int sock)
 {
     sendall(sock, "M", 1);
-    recvall(sock, &map_data, sizeof(map_data));
+    struct map_info map_info_net;
+    recvall(sock, &map_info_net, sizeof(map_info_net));
+    map_data = net_to_map_info(&map_info_net);
     g_map = generate_map(map_data);
 }
