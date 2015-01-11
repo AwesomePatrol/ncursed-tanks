1a0c7385e89ffcaceed8ba7f18588a5ff9d0caae
Aleksander Mistewicz <a.mistewicz@gmail.com>
2015-01-11 23:13:11 +0100

    client: Now bullet explode at position provided by server.

--- client_render.c
+++ client_render.c
@@ -141,6 +141,8 @@ void render_shot(struct shot *shot, int s_id)
         if (map_pos.x > map_data->length  || map_pos.x < 0)
             break;
         if (t > g_impact_t) {
+            b_pos = shot_pos(init_pos, init_v, acc, g_impact_t);
+            map_pos = round_to_map_pos(b_pos);
             draw_bullet_explosion(dx, dy, map_pos.x, map_pos.y);
             refresh();
             break;
