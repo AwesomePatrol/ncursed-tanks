67dd4e94bfb29d188e118acb8fedb019442e5616
Aleksander Mistewicz <a.mistewicz@gmail.com>
2015-01-15 16:18:59 +0100

    abilities: Merge abilities_config with config

--- config.c
+++ config.c
@@ -13,6 +13,7 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 FILE *config_file = NULL;
+FILE *a_config_file = NULL;
 
 struct config_item config[] = {
     {"map_width", 128, 48, 1024},
@@ -27,16 +28,31 @@ struct config_item config[] = {
     {"tank_distance", 10, 1, 128},
 };
 
-const int config_count = ARRAY_SIZE(config);
+struct a_config_item abilities[] = {
+    {"double_shot", 3, 50},
+    {"move", 3, 5},
+    {"sniper", 4, 0},
+};
 
+const int config_count = ARRAY_SIZE(config);
+const int a_config_count = ARRAY_SIZE(abilities);
 
 char *read_line(FILE *stream);
 char *read_delimited(FILE *stream, int delim);
 
 /*
- * Config file format:
+ * server.conf file format:
  * name1 value1
  * name2 value2
+ * ...
+ *
+ * (Newline at end of file mandatory)
+ */
+
+/*
+ * abilities.conf file format:
+ * name1 type1 cooldown1 value1
+ * name2 type2 cooldown2 value2
  * ...
  *
  * (Newline at end of file mandatory)
@@ -80,6 +96,46 @@ void read_config()
     }
 }
 
+void a_read_config()
+{
+    /* TODO error checking */
+    a_config_file = fopen(ABILITIES_CONFIG_FILENAME, "r");
+    if (a_config_file != NULL)
+    {
+        for (int i = 0; i < a_config_count; i++)
+        {
+            char *name;
+            char *value_s;
+            config_value_t value1;
+            config_value_t value2;
+
+            /* read name */
+            name = read_delimited(a_config_file, ' ');
+            if (!name)
+                break;
+
+            /* read value */
+            value_s = read_line(a_config_file);
+            if (!value_s)
+                break;
+            /* TODO what if the number is huge?
+             * Change value type to int16_t
+             */
+            if (sscanf(value_s, "%d %d", &value1, &value2) != 1)
+                break;
+            free(value_s);
+
+            debug_s(0, "a_config: read name", name);
+            debug_d(0, "a_config: read value1", value1);
+            debug_d(0, "a_config: read value2", value2);
+            a_config_set(name, value1, value2);
+
+            free(name);
+        }
+        fclose(a_config_file);
+    }
+}
+
 void write_config()
 {
     config_file = fopen(SERVER_CONFIG_FILENAME, "w");
@@ -89,12 +145,30 @@ void write_config()
     fclose(config_file);
 }
 
+void a_write_config()
+{
+    a_config_file = fopen(ABILITIES_CONFIG_FILENAME, "w");
+    for (int i=0; i<a_config_count; i++)
+        fprintf(a_config_file, "%s %d %d\n",
+                abilities[i].name, abilities[i].cooldown, abilities[i].param);
+    fclose(a_config_file);
+}
+
 config_value_t config_get(char *name)
 {
     /* TODO Do something about the linear search. Use a hash table? */
     for (int i = 0; i < config_count; i++)
         if (strcmp(config[i].name, name) == 0)
             return config[i].value;
+    return 0; /* If nothing found. Not the best way to show it */
+}
+
+struct a_config_item *a_config_get(char *name)
+{
+    /* TODO Do something about the linear search. Use a hash table? */
+    for (int i = 0; i < a_config_count; i++)
+        if (strcmp(abilities[i].name, name) == 0)
+            return &abilities[i];
     return 0; /* If nothing found. Not the best way to show it */
 }
 
@@ -107,6 +181,23 @@ void config_set(char *name, config_value_t value)
         if (strcmp(name, config[i].name) == 0)
         {
             config[i].value = value;
+            return;
+        }
+    }
+    /* No such name found. Doing nothing about it for now. */
+    debug_s(5, "config_set: No option found in config", name);
+}
+
+void a_config_set(char *name, config_value_t value1, config_value_t value2)
+{
+    /* find a config item whose name matches this name
+     * and place the value there */
+    for (int i = 0; i < a_config_count; i++)
+    {
+        if (strcmp(name, abilities[i].name) == 0)
+        {
+            abilities[i].cooldown = value1;
+            abilities[i].param = value2;
             return;
         }
     }
--- config.h
+++ config.h
@@ -13,7 +13,15 @@ struct config_item
     config_value_t max;
 };
 
+struct a_config_item
+{
+    char *name;
+    config_value_t cooldown;
+    config_value_t param;
+};
+
 #define SERVER_CONFIG_FILENAME "server.conf"
+#define ABILITIES_CONFIG_FILENAME "abilities.conf"
 
 extern struct config_item config[];
 extern const int config_count;
@@ -23,5 +31,14 @@ void write_config();
 
 int config_get(char *name);
 void config_set(char *name, config_value_t value);
+
+extern struct a_config_item abilities[];
+extern const int a_config_count;
+
+void a_read_config();
+void a_write_config();
+
+struct a_config_item *a_config_get(char *name);
+void a_config_set(char *name, config_value_t value1, config_value_t value2);
 
 #endif /* CONFIG_H */
